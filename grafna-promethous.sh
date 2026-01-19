f24eef77ff0c4f6be24148258f28ea4d7a203d04
NALUBOLU RAJASEKHAR <nalubolurajasekhar7386@gmail.com>
2026-01-19 12:43:35 +0530

    Update grafna-promethous.sh

--- grafna-promethous.sh
+++ grafna-promethous.sh
@@ -137,7 +137,7 @@ route:
 receivers:
   - name: pagerduty
     pagerduty_configs:
-      - routing_key: "4bf6bcd8cb9b4607d047c4b6c2d333d6" #add key pagerduty 
+      - routing_key: "6dda0ce469054906c0f679382f6de567" #add key pagerduty 
         severity: "critical"
 EOF
 
